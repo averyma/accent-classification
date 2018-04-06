839fac644557483470ad01ff899c3aba34ba4849
Avery Ma <avery.mabojie@gmail.com>
2018-04-05 21:33:36 -0400

    converting mp3 files to wav

---
+++ MP3toWAV.py
@@ -0,0 +1,13 @@
+import csv
+from pydub import AudioSegment
+
+with open('dummy.csv', newline='') as f:
+    reader = csv.reader(f)
+    filenames = [row[0] for row in reader]
+    filenames = filenames[1:]
+#     print(filenames)
+    
+    for file in filenames:
+        
+        sample_mp3 = AudioSegment.from_mp3(file)
+        sample_mp3.export("wav/sample-" + str(file[21:-5]) + ".wav", format="wav")
