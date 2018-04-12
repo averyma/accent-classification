13c9c300a3bfd91f199ad3740e9d880e957e89f0
Avery Ma <avery.mabojie@gmail.com>
2018-04-12 15:35:21 -0400

    moved to obsolete

--- mp3_to_wav.py
+++ mp3_to_wav.py
@@ -1,3 +1,6 @@
+# Convert mp3 audio data to much preferred wav files.
+# No longer needed since it was handled during the generate_utterance stgage.
+
 import csv
 from pydub import AudioSegment
 
@@ -8,6 +11,5 @@ with open('dummy.csv', newline='') as f:
 #     print(filenames)
     
     for file in filenames:
-        
         sample_mp3 = AudioSegment.from_mp3(file)
         sample_mp3.export("wav/sample-" + str(file[21:-5]) + ".wav", format="wav")
